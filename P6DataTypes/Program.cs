1ca17cadfdc3f9820bf2e0c79f41ecea4bbadaf9
reynin <angelo.panghulan@student.forsbergsskola.se>
2023-09-05 13:20:29 +0200

    P6DataType

---
+++ P6DataTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿//A string type variable is created, with a declared value containing text.
+string outputString = "Hello World!";
+Console.WriteLine(outputString);
+
+//A float type variable, with a declared value containing numbers with decimals.
+float decimalValue = 0.3f;
+Console.WriteLine(decimalValue);
+
+//A bool type variable, which can only contain one of two values, true or false.
+bool isAwsome = true;
+Console.WriteLine(isAwsome);
+
+/*
+ This is a byte type variable, which contain byte data.
+ Unlike the variable int, which store only up to 4 byte, this type store up to 8.
+ In other words, it can contain higher numbers
+ */
+byte hexByte = 0xa7;
+Console.WriteLine(hexByte);

bf9186c5d5017b5cb9f2e88e5cdd87015dce30e8
reynin <angelo.panghulan@student.forsbergsskola.se>
2023-09-05 10:54:53 +0200

    P5Variables

---
+++ P5Variables/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = 5;
+//Via debugging, the variable is created but not yet declared
+//A variable is created and also declared with the value 5
+Console.WriteLine(number);
+//The console will now print out the 5
+number = 10;
+//Via debugging, the variables value is still 5
+//The variables value is now declared/replacing with a new value, 10
+Console.WriteLine(number);
+//The console will now print out the new variable, 10

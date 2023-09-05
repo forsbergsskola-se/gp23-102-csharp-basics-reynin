d01397f5b5692d35f196307e1e14cbdff6727e08
reynin <angelo.panghulan@student.forsbergsskola.se>
2023-09-05 14:38:50 +0200

    P8Conversion

--- P7Input/Program.cs
+++ P7Input/Program.cs
@@ -3,8 +3,8 @@ Console.WriteLine("What's your name?");
 
 //A string type is created
 //Console awaits user to write a name
-//The input is now declared as a value for the string type
-string name = Console.ReadLine();
+//The input is now returned as a declared value for the string type
+string? name = Console.ReadLine();
 
 //Console prints out "Hello " and the users input
 Console.WriteLine($"Hello, {name}!");
---
+++ P8Conversion/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Give me a number.");
+string input = Console.ReadLine();
+double i = Convert.ToDouble(input);
+Console.WriteLine();

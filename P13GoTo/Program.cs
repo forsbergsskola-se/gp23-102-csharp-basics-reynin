60a98a885626992a4e9878d3ae9f0eea78f865fe
reynin <angelo.panghulan@student.forsbergsskola.se>
2023-09-13 10:40:42 +0200

    Update Program.cs

--- P13GoTo/Program.cs
+++ P13GoTo/Program.cs
@@ -3,22 +3,34 @@
 int compNumber = random.Next(1,101);
 
 Console.WriteLine("I have picked a number (1-100). It's your turn to guess it!");
+Console.WriteLine("You don't need more than 7 guesses.");
+Console.WriteLine("You lose after 10 guesses.");
 
+int tryCounter = 0;
 Guess:
+tryCounter++;
 
 int inputNumber = int.Parse(Console.ReadLine());
 
-if (compNumber > inputNumber)
+if (tryCounter < 10)
 {
-    Console.WriteLine("Nope! My number is Greater!");
-    goto Guess;
-}
-else if(compNumber < inputNumber)
-{
-    Console.WriteLine("Nope! My number is Smaller!");
-    goto Guess;
+
+    if (compNumber > inputNumber)
+    {
+        Console.WriteLine("Nope! My number is Greater!");
+        goto Guess;
+    }
+    else if (compNumber < inputNumber)
+    {
+        Console.WriteLine("Nope! My number is Smaller!");
+        goto Guess;
+    }
+    else
+    {
+        Console.WriteLine("Output: That's the number! Well played!");
+    }
 }
 else
 {
-    Console.WriteLine("Output: That's the number! Well played!");
+    Console.WriteLine("You Lose!");
 }

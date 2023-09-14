9af793d5c26e2b49135f76943307acef8b3e8665
reynin <angelo.panghulan@student.forsbergsskola.se>
2023-09-14 09:07:18 +0200

    Nim

--- Nim/Program.cs
+++ Nim/Program.cs
@@ -9,6 +9,7 @@ Console.WriteLine("Player 1: How many matches do you want to draw?");
 int playerDraw;
 
 round:
+
 playerDraw = int.Parse(Console.ReadLine());
 
 if (matches == playerDraw) //Game Over if player loses
@@ -16,40 +17,37 @@ if (matches == playerDraw) //Game Over if player loses
     Console.WriteLine("\nYou lose!");
     goto theEnd;
 }
-    
-matches -= playerDraw;
+matches -= playerDraw; //Total matches left after players turn
 
-int matchCount = 0;
+int matchCount = 0;  //The printout
 displayMatches:
 matchCount++;
-if (matchCount <= matches) //The printout
+if (matchCount <= matches)
 {
     Console.Write("|");
     goto displayMatches;
 }
-
 Console.Write($" ({matches})\n");
 
-int drawAI = Random.Shared.Next(1, 4);
-
+int drawAI = Random.Shared.Next(1, 4); //Ai's turn
 Console.WriteLine($"Ai draws {drawAI} matches.");
 
 matches -= drawAI;
 
-matchCount = 0;
+matchCount = 0; //The printout
 displayMatchesAi:
 matchCount++;
-if (matchCount <= matches) //The printout
+if (matchCount <= matches)
 {
     Console.Write("|");
     goto displayMatchesAi;
 }
 
-if (matches > 0)
+if (matches > 0) //Round starts anew
 {
-    Console.Write($" ({matches})\n");
+    Console.Write($" ({matches})\n"); //Total matches left after AI's turn
     goto round;
 }
 
-Console.WriteLine("\nAi lose!");//Game Over when ai loses
+Console.WriteLine("\nAi lose!"); //Game Over when ai loses
 theEnd: ;
--- P13GoTo/Program.cs
+++ P13GoTo/Program.cs
@@ -1,6 +1,4 @@
-﻿Random random = new Random();
-
-int compNumber = random.Next(1,101);
+﻿int compNumber = Random.Shared.Next(1,101);
 
 Console.WriteLine("I have picked a number (1-100). It's your turn to guess it!");
 Console.WriteLine("You don't need more than 7 guesses.");
@@ -14,21 +12,17 @@ int inputNumber = int.Parse(Console.ReadLine());
 
 if (tryCounter < 10)
 {
-
     if (compNumber > inputNumber)
     {
         Console.WriteLine("Nope! My number is Greater!");
         goto Guess;
     }
-    else if (compNumber < inputNumber)
+    if (compNumber < inputNumber)
     {
         Console.WriteLine("Nope! My number is Smaller!");
         goto Guess;
     }
-    else
-    {
-        Console.WriteLine("Output: That's the number! Well played!");
-    }
+    Console.WriteLine("That's the number! Well played!");
 }
 else
 {

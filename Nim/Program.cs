8350d3bd197171d3b09c24e0e2e5bed2d2b8231f
reynin <144011447+reynin@users.noreply.github.com>
2023-09-15 22:47:00 +0200

    Update Program.cs

--- Nim/Program.cs
+++ Nim/Program.cs
@@ -1,141 +1,64 @@
-﻿void basicNim()
+﻿Console.WriteLine("Welcome to Nim");
+Console.WriteLine("Rules are: You may only draw up to 3 matches.");
+
+int matches = 24; //Starting matches
+
+Console.WriteLine("|||||||||||||||||||||||| (24)"); //Presenting total matches
+Console.WriteLine("Player 1: How many matches do you want to draw?");
+
+round:
+tryAgain:
+
+int playerDraw = int.Parse(Console.ReadLine());
+
+if (playerDraw > matches)
 {
-    Console.WriteLine("Welcome to Nim");
-    Console.WriteLine("Rules are: You may only draw up to 3 matches.");
-
-    int matches = 24; //Starting matches
-
-    Console.WriteLine("|||||||||||||||||||||||| (24)"); //Presenting total matches
-    Console.WriteLine("Player 1: How many matches do you want to draw?");
-
-    round:
-    tryAgain:
-
-    int playerDraw = int.Parse(Console.ReadLine());
-
-    if (playerDraw > matches)
-    {
-        Console.WriteLine("You can't draw more than there is matches, try again!");
-        goto tryAgain;
-    }
-
-    if (matches == playerDraw) //Game Over if player loses
-    {
-        Console.WriteLine("\nYou drew the last match. You lose!");
-        goto theEnd;
-    }
-
-    matches -= playerDraw; //Total matches left after players turn
-
-    int matchCount = 0; //The printout
-    displayMatches:
-    matchCount++;
-    if (matchCount <= matches)
-    {
-        Console.Write("|");
-        goto displayMatches;
-    }
-
-    Console.Write($" ({matches})\n");
-
-    int drawAI = Random.Shared.Next(1, 4); //Ai's turn
-    Console.WriteLine($"Ai draws {drawAI} matches.");
-
-    matches -= drawAI;
-
-    matchCount = 0; //The printout
-    displayMatchesAi:
-    matchCount++;
-    if (matchCount <= matches)
-    {
-        Console.Write("|");
-        goto displayMatchesAi;
-    }
-
-    if (matches > 0) //Round starts anew
-    {
-        Console.Write($" ({matches})\n"); //Total matches left after AI's turn
-        goto round;
-    }
-
-    Console.WriteLine("\nAi drew the last match. Ai lose!"); //Game Over when ai loses
-    theEnd: ;
+    Console.WriteLine("You can't draw more than there is matches, try again!");
+    goto tryAgain;
 }
 
-//With smarter ai
-//If total matches is odd, ai picks even, and vice versa
-void smartAiNim()
+if (matches == playerDraw) //Game Over if player loses
 {
-    Console.WriteLine("Welcome to Nim");
-    Console.WriteLine("Rules are: You may only draw up to 3 matches.");
-    
-    int matches = 24; //Starting matches
-    
-    Console.WriteLine("|||||||||||||||||||||||| (24)"); //Presenting total matches
-    Console.WriteLine("Player 1: How many matches do you want to draw?");
-    
-    round:
-    tryAgain:
-    
-    int playerDraw = int.Parse(Console.ReadLine());
-    
-    if (playerDraw > matches)
-    {
-        Console.WriteLine("You can't draw more than there is matches, try again!");
-        goto tryAgain;
-    }
-    
-    if (matches == playerDraw) //Game Over if player loses
-    {
-        Console.WriteLine("\nYou drew the last match. You lose!");
-        goto theEnd;
-    }
-    matches -= playerDraw; //Total matches left after players turn
-    
-    int matchCount = 0; //The printout
-    displayMatches:
-    matchCount++;
-    if (matchCount <= matches)
-    {
-        Console.Write("|");
-        goto displayMatches;
-    }
-    Console.Write($" ({matches})\n");
-    
-    int drawAI = playerDraw == 1 ? 3 : playerDraw == 2 ? 2 : 1; //SmartAI turn
-    
-    if (matches <= 12)
-    {
-        if (playerDraw == 1)
-        {
-            drawAI = 2;
-        } 
-        else if(playerDraw == 2)
-        {
-            drawAI = 1;
-        }
-    }
-    // 1 2 3 4 5 6 7 8 9 10 11 12
-    
-    Console.WriteLine($"Ai draws {drawAI} matches.");
-    
-    matches -= drawAI;
-    matchCount = 0; //The printout
-    displayMatchesAi:
-    matchCount++;
-    if (matchCount <= matches)
-    {
-        Console.Write("|");
-        goto displayMatchesAi;
-    }
-    
-    if (matches > 0) //Round starts anew
-    {
-        Console.Write($" ({matches})\n"); //Total matches left after AI's turn
-        goto round;
-    }
-    
-    Console.WriteLine("\nAi drew the last match. Ai lose!"); //Game Over when ai loses
-    theEnd: ;
+    Console.WriteLine("\nYou drew the last match. You lose!");
+    goto theEnd;
 }
-smartAiNim();
+
+matches -= playerDraw; //Total matches left after players turn
+
+int matchCount = 0; //The printout
+displayMatches:
+matchCount++;
+if (matchCount <= matches)
+{
+    Console.Write("|");
+    goto displayMatches;
+}
+
+Console.Write($" ({matches})\n");
+
+//Ai's turn
+    
+int drawAI = matches % 4 == 0 ? 3 : matches % 2 != 0 ? 2 : 1; //With smarter AI; if ai is in certain position, it will try to put player in losing position (5, 9, 13, 17, 21)
+                                                              // Basic way: int drawAI = Random.Shared.Next(1, 4);
+    
+Console.WriteLine($"Ai draws {drawAI} matches.");
+
+matches -= drawAI;
+
+matchCount = 0; //The printout
+displayMatchesAi:
+matchCount++;
+if (matchCount <= matches)
+{
+    Console.Write("|");
+    goto displayMatchesAi;
+}
+
+if (matches > 0) //Round starts anew
+{
+    Console.Write($" ({matches})\n"); //Total matches left after AI's turn
+    goto round;
+}
+
+Console.WriteLine("\nAi drew the last match. Ai lose!"); //Game Over when ai loses
+theEnd: ;
